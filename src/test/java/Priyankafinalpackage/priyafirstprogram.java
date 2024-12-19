2fb968774d397025546c57430650d114eafdf014
testingksty <testingksty2010@gmail.com>
2024-12-19 21:34:14 +0000

    Update priyafirstprogram.java

--- src/test/java/Priyankafinalpackage/priyafirstprogram.java
+++ src/test/java/Priyankafinalpackage/priyafirstprogram.java
@@ -7,7 +7,7 @@ public class priyafirstprogram {
 	public static void main(String[] args) {
 		ChromeDriver driver = new ChromeDriver();
 		driver.get("https://www.facebook.com");
-
+System.out.println("hi friends");
 	}
 
 }
