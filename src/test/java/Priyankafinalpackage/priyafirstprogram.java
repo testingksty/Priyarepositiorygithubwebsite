73365815bc313034cd27d0216347b499663f9fef
vas070707 <vas070707@gmail.com>
2024-12-19 21:32:32 +0000

    successfully saved before mvoing to cloud

---
+++ src/test/java/Priyankafinalpackage/priyafirstprogram.java
@@ -0,0 +1,13 @@
+package Priyankafinalpackage;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class priyafirstprogram {
+
+	public static void main(String[] args) {
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com");
+
+	}
+
+}
