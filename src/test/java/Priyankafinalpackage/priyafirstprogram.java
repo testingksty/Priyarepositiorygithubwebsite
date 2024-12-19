259a7bb2575ce799c0b154ad9dd4fe4fb9889759
testingksty <testingksty2010@gmail.com>
2024-12-19 21:53:48 +0000

    Update priyafirstprogram.java

--- src/test/java/Priyankafinalpackage/priyafirstprogram.java
+++ src/test/java/Priyankafinalpackage/priyafirstprogram.java
@@ -1,3 +1,4 @@
+
 package Priyankafinalpackage;
 
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,7 +9,7 @@ public class priyafirstprogram {
 	public static void main(String[] args) throws InterruptedException {
 		ChromeDriver driver = new ChromeDriver();
 		driver.get("https://www.facebook.com");
-		Thread.sleep(5000);
+		Thread.sleep(10000);
 		System.out.println("all the best");
 	//am using firefox
 		FirefoxDriver driver = new FirefoxDriver();
